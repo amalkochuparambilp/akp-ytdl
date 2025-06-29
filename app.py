3d02bba6e630781022215a9a4736a66741e7ad99
amalkochuparambilp <98939651+amalkochuparambilp@users.noreply.github.com>
2025-06-29 17:15:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("🌀 Welcome to AKP Downloader")
 st.markdown("Choose a downloader:")
 
 if st.button("🎬 YouTube Downloader"):
-    st.switch_page("pages/1_Home")
+    st.switch_page("1_Home")
 
 if st.button("📸 Instagram Reels Downloader"):
-    st.switch_page("pages/2_Instagram")
+    st.switch_page("2_Instagram")
